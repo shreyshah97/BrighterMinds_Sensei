417156b2d22db3848f4e004fa6b845f60b77420a
Shrey Shah <shreybshah.97.ss@gmail.com>
2018-09-10 13:59:51 +0530

    Uploaded all files

---
+++ app/build/generated/source/buildConfig/androidTest/debug/org/brighterminds/relaxationapp/test/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.brighterminds.relaxationapp.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.brighterminds.relaxationapp.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 4;
+  public static final String VERSION_NAME = "1.4";
+  // Fields from build type: debug
+  public static final String SERVER_URL = "http://52.87.234.182/esquare-server";
+}
